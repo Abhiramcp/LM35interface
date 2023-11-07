903712aae457224d90f3aaae5419644a9582fd0b
abhiramcp <abhiramcp1243@gmail.com>
2023-11-08 01:04:51 +0530

    formatted

--- Temperature_sensor.ino
+++ Temperature_sensor.ino
@@ -15,28 +15,28 @@ void setup() {
 
 void loop()
 {
-  if (temperatureTimer >= 1000) 
+  if (temperatureTimer >= 1000)
   {
     temperatureTimer = 0;
-    
+
     int reading = analogRead(sensorPin);
     float voltage = reading * (5.0 / 1024.0);
     float temperatureC = voltage * 100;
 
-   Serial.print("Temperature:");
-   Serial.print(temperatureC);
-   Serial.print("\xC2\xB0");
-   Serial.print("C ");
-   Serial.println(".");
+    Serial.print("Temperature:");
+    Serial.print(temperatureC);
+    Serial.print("\xC2\xB0");
+    Serial.print("C ");
+    Serial.println(".");
 
-    if (temperatureC < 30) 
-     {
+    if (temperatureC < 30)
+    {
       ledBlinkInterval = 250; // Set the LED blink interval to 250 ms if temperature is below 30°C
-     } 
-    else 
-     {
+    }
+    else
+    {
       ledBlinkInterval = 500; // Set the LED blink interval to 500 ms if temperature is 30°C or higher
-     }
+    }
   }
 
   if (ledBlinkTimer >= ledBlinkInterval) {
